dce3d6dcc1363419ecf773b18f63bc71b48f918c
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-09-29 11:09:26 +0200

    decent start that exists

---
+++ eleventy.config.js
@@ -0,0 +1,13 @@
+export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("img");
+    
+    eleventyConfig.addShortcode("formatDate", () => {
+        return new Date().toISOString().split('sv-SE')[0]
+    })
+
+
+    return{
+        markdownTemplateEngine:"njk",
+    }
+};
